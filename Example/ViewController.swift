3ae42f3bcb3d1609971eea6f63dbe0471a438be3
tuanha15 <tuanha15@vpbank.com.vn>
2024-11-15 10:22:05 +0700

    Update example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,99 +8,61 @@
 import UIKit
 import PhotosUI
 import DocumentScanner
+import PDFKit
 
-class ViewController: UIViewController, PHPickerViewControllerDelegate {
+class ViewController: UIViewController {
     
     var documentScanner = DocumentScanner()
     var selectedImages: [UIImage] = []
+    var activityIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Tạo Activity Indicator
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = UIColor { traitCollection in
+            return traitCollection.userInterfaceStyle == .dark ? .white : .gray
+        } // Màu sắc tùy chỉnh
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true // Tự ẩn khi dừng
+        
+        // Thêm Indicator vào View
+        view.addSubview(activityIndicator)
     }
 
     @IBAction func onPressedCameraBtn(_ sender: UIButton) {
-        documentScanner.openScannerCamera(from: self) {pdfPaths in
-            for path in pdfPaths {
-                print("pdfPath: \(path)")
-            }
+        documentScanner.openScannerCamera(from: self) {pdfUrl in
+            print("pdfPath: \(pdfUrl?.path)")
             
+
+            // Kiểm tra và hiển thị file PDF
+            if pdfUrl != nil {
+                // Thiết lập PDFView
+                let pdfView = PDFView(frame: self.view.bounds)
+                pdfView.autoScales = true
+                self.view.addSubview(pdfView)
+                pdfView.document = PDFDocument(url: pdfUrl!)
+            } else {
+                print("PDF URL is nil")
+            }
         }
     }
     
     @IBAction func onPressedPhotosBtn(_ sender: UIButton) {
-        openImagePicker()
-    }
-    
-    func openImagePicker() {
-        // Cấu hình PHPicker cho phép chọn nhiều ảnh
-        var config = PHPickerConfiguration()
-        config.filter = .images // Chỉ hiển thị ảnh
-        config.selectionLimit = 0 // Cho phép chọn không giới hạn ảnh
-        
-        let picker = PHPickerViewController(configuration: config)
-        picker.delegate = self
-        present(picker, animated: true, completion: {
-                print("Open Image Picker, complete")
-        })
-    }
-
-    // Hàm delegate được gọi khi người dùng chọn xong ảnh
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        
-        selectedImages.removeAll() // Làm rỗng danh sách trước khi thêm ảnh mới
-        
-        // Tạo biến để theo dõi số lượng ảnh đã tải
-        var loadedImageCount = 0
-        let totalImages = results.count
-    
-        for result in results {
-            if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
-                
-                result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (image, error) in
-                    if let image = image as? UIImage {
-                        DispatchQueue.main.async {
-                            self?.selectedImages.append(image) // Thêm ảnh vào mảng
-                            loadedImageCount += 1 // Tăng số lượng ảnh đã tải xong
-
-                            // Kiểm tra nếu tất cả ảnh đã tải xong
-                            if loadedImageCount == totalImages {
-                                picker.dismiss(animated: true) {
-                                    self?.onPushToImageListView()
-                                }
-                            }
-                        }
-                        
-                    } else {
-                        // Nếu gặp lỗi khi tải ảnh, vẫn tăng loadedImageCount để tránh treo
-                        DispatchQueue.main.async {
-                            loadedImageCount += 1
-                            if loadedImageCount == totalImages {
-                                picker.dismiss(animated: true) {
-                                    self?.onPushToImageListView()
-                                }
-                            }
-                        }
-                    }
-                }
-            } else {
-                loadedImageCount += 1
-            }
-        }
-        
-        // Nếu results rỗng, đóng picker ngay lập tức
-        if results.isEmpty {
-            picker.dismiss(animated: true) {
-                self.onPushToImageListView()
-            }
-        }
-    }
-    
-    func onPushToImageListView() {
-        documentScanner.pushToImageListView(images: selectedImages, from: self) {pdfPaths in
+        DocumentScanner.shared.openScannerPhotosLibrary(images: nil, from: self) {pdfUrl in
             print("ViewController.onPushToImageListView")
-            for path in pdfPaths {
-                print("pdfPath: \(path)")
+            print("pdfPath: \(pdfUrl?.path)")
+            
+
+            // Kiểm tra và hiển thị file PDF
+            if pdfUrl != nil {
+                // Thiết lập PDFView
+                let pdfView = PDFView(frame: self.view.bounds)
+                pdfView.autoScales = true
+                self.view.addSubview(pdfView)
+                pdfView.document = PDFDocument(url: pdfUrl!)
+            } else {
+                print("PDF URL is nil")
             }
         }
     }
